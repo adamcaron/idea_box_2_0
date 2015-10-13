66236d48368b84c75ce6c2b5741bdd16f403eb8e
Adam Caron <adamwcaron@gmail.com>
2015-10-12 22:22:25 -0600

    Setup homepage view

---
+++ spec/features/user_sees_ideas_on_landing_page_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+feature "User visits homepage" do
+  xscenario "and " do
+    # visit root_path
+
+    # expect(page).to have_content("There aren't any ideas, yet... You should add some!")
+  end
+end

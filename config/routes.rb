7f6e0ec55942193380819a508de8dd83c341055c
Adam Caron <adamwcaron@gmail.com>
2015-10-14 15:52:24 -0600

    Add feature to Edit an Idea

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root   'ideas#index'
-  post   '/ideas',     to: 'ideas#create'
-  delete '/ideas/:id', to: 'ideas#destroy'
+  # post   '/ideas',          to: 'ideas#create'
+  # delete '/ideas/:id',      to: 'ideas#destroy'
+  # get    '/ideas/:id/edit', to: 'ideas#edit', as: 'edit_idea'
+  # post   '/ideas/:id',      to: 'ideas#update'
+
+  resources :ideas, except: [:new, :index, :show]
 end

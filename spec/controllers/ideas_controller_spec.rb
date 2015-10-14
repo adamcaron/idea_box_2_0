60bbfb228036c456a40dff1684ff7dc9d9425b8f
Adam Caron <adamwcaron@gmail.com>
2015-10-13 21:16:29 -0600

    Test Idea creation

--- spec/controllers/ideas_controller_spec.rb
+++ spec/controllers/ideas_controller_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
-RSpec.describe IdeaController, type: :controller do
+RSpec.describe IdeasController, type: :controller do
+  context "#create" do
+    it "responds with a newly-created idea" do
+      title      = "Best. Idea. Ever."
+      body       = "This is the body and description for my idea."
+      idea_count = Idea.count
 
+      post :create, format: :json, title: title, body: body
+
+      new_idea       = JSON.parse(response.body, symbolize_names: true)
+      new_idea_count = Idea.count
+
+      # Creates an idea in the database
+      expect(new_idea_count).to eq(idea_count + 1)
+
+      # The response contains the new idea's information
+      expect(response).to have_http_status(:success)
+      expect(new_idea[:title]).to eq("Best. Idea. Ever.")
+      expect(new_idea[:body]).to eq("This is the body and description for my idea.")
+      expect(new_idea[:quality]).to eq("swill")
+    end
+  end
 end

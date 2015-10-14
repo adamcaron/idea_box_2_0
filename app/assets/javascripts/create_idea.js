5a3c20086e7fabe7ec6e202ec4036b1c30793bb0
Adam Caron <adamwcaron@gmail.com>
2015-10-13 20:55:20 -0600

    Add feature to create idea
    render newly-created idea asynchronously

---
+++ app/assets/javascripts/create_idea.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+  $("#idea-creation-area").delegate("#save-idea", "click", function (event){
+    event.preventDefault();
+    var title = $("#idea_title").val();
+    var body  = $("#idea_body").val();
+    var form  = '#idea-creation-area form'
+    createNewIdea(title, body);
+    clearFormFields(form);
+  });
+});
+
+function createNewIdea(title, body) {
+  $.ajax({
+    type: "POST",
+    url: "/idea/create",
+    data: { title: title, body: body },
+    success: function(newIdea) {
+      renderNewIdea(newIdea);
+    }
+  });
+};
+
+function clearFormFields(form) {
+  $(form).each(function(){
+    this.reset();
+  });
+};
+
+function renderNewIdea(idea) {
+  $(".idea").first().before(
+    "<article class='idea'><h2>"
+    + idea.title
+    + "</h2><span>"
+    + idea.date
+    + "</span><p>"
+    + idea.body
+    + "</p><span>"
+    + idea.quality
+    + "\n</span><button name='button' type='submit'>+</button>\n"
+    + "</span><button name='button' type='submit'>-</button>\n"
+    + "</span><button name='button' type='submit'>Edit</button>\n"
+    + "</span><button name='button' type='submit'>Delete</button>"
+    + "</article>"
+  );
+};

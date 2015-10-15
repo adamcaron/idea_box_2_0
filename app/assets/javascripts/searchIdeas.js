f741c1e1efe788fd1bf15714b8aaa184348c08ba
Adam Caron <adamwcaron@gmail.com>
2015-10-15 00:40:30 -0600

    Add Search functionality
    user can search for ideas
    and view results asynchronously

---
+++ app/assets/javascripts/searchIdeas.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+  filterSearchInput();
+});
+
+function filterSearchInput () {
+  // When user types something
+  $("#search-input").keyup(function () {
+    var input = $("#search-input").val().toLowerCase()
+    // Find search results
+    renderSearchResults(input);
+  });
+};
+
+function renderSearchResults (searchInput) {
+  // Compare all the ideas with Search Input
+  $(".idea").each(function (index, idea) {
+    var ideaContent = $(idea).find('h2, p').text().toLowerCase()
+    // Compare content with search terms
+    var results = ideaContent.indexOf(searchInput) !== -1;
+    // hide or show accordingly
+    $(idea).toggle(results)
+  });
+};
--- spec/features/user_sees_ideas_on_landing_page_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-feature "User visits homepage" do
-  xscenario "and " do
-    # visit root_path
-
-    # expect(page).to have_content("There aren't any ideas, yet... You should add some!")
-  end
-end
